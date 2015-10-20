696ecf51c0b6051e1b24c709c11e19cbe8c7865d
duyltv <ngocduy1842@gmail.com>
2015-10-20 10:23:15 +0700

    Fix saoluu.php

--- Functions/Saoluu/saoluu.php
+++ Functions/Saoluu/saoluu.php
@@ -17,15 +17,6 @@
 	echo '</head>'.PHP_EOL;
 	echo '<meta charset="UTF-8">'.PHP_EOL;
 	
-	# Process submittion
-	if(isset($_POST['bckFile'])){
-		echo '<div id="progressBar" class="tiny-green"><div></div></div>';
-		echo '<script>';
-		echo 'progressBar(75, $("#progressBar"));';
-		echo '</script>';
-	} else if(isset($_POST['bckData'])){
-		
-	}
 	
 	# Interface
 	echo 'BACKUP SYSTEM<br>'.PHP_EOL;
@@ -36,6 +27,19 @@
 	echo '</td>'.PHP_EOL;
 	echo '<td>'.PHP_EOL;
 	echo '<input type="submit" name="bckData" value="Backup database">'.PHP_EOL;
+	echo '</td>'.PHP_EOL;
+	echo '<td>'.PHP_EOL;
+	
+	# Process submittion
+	if(isset($_POST['bckFile'])){
+		echo '<div id="progressBar" class="tiny-green"><div></div></div>';
+		echo '<script>';
+		echo 'progressBar(75, $("#progressBar"));';
+		echo '</script>';
+	} else if(isset($_POST['bckData'])){
+		
+	}
+	
 	echo '</td>'.PHP_EOL;
 	echo '</tr>'.PHP_EOL.'</table>';
 	echo '</form>';
