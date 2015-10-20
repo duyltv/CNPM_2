bffb9507b9c78f34f5504d3d5b8bf61101b002ec
duyltv <ngocduy1842@gmail.com>
2015-10-20 10:05:18 +0700

    Upload saoluu.php

---
+++ Functions/Saoluu/saoluu.php
@@ -0,0 +1,27 @@
+ <!-- 
+ * AgriExtension
+ * File: 	saoluu.php
+ * Written: Nguyen Ngoc Duy
+ * Date: Oct 20 2015
+ -->
+
+<?php
+	# Set html envirolment
+	echo "<!DOCTYPE html>".PHP_EOL;
+	echo "<html>".PHP_EOL;
+	echo '<meta charset="UTF-8">'.PHP_EOL;
+	
+	echo 'BACKUP SYSTEM<br>'.PHP_EOL;
+	echo '<form action="saoluu.php" method="post">'.PHP_EOL;
+	echo '<table>'.PHP_EOL.'<tr>'.PHP_EOL;
+	echo '<td>'.PHP_EOL;
+	echo '<input type="hidden" name="bckFile" value="Backup files">';
+	echo '</td>'.PHP_EOL;
+	echo '<td>'.PHP_EOL;
+	echo '<input type="hidden" name="bckData" value="Backup database">';
+	echo '</td>'.PHP_EOL;
+	echo '</tr>'.PHP_EOL.'</table>';
+	echo '</form>';
+	
+	echo '</html>';
+?>
