be4e92520cbb88817689ba4dcd8c0a6b67f0616a
duyltv <ngocduy1842@gmail.com>
2015-10-20 10:21:18 +0700

    Upload jquery code

--- Functions/Saoluu/saoluu.php
+++ Functions/Saoluu/saoluu.php
@@ -9,8 +9,25 @@
 	# Set html envirolment
 	echo "<!DOCTYPE html>".PHP_EOL;
 	echo "<html>".PHP_EOL;
+	echo '<head>'.PHP_EOL;
+	echo '<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"></script>';
+	echo '<script type="text/javascript" src="progressbar.js"></script>';
+	echo '<link rel="stylesheet" type="text/css" href="progressbar.css">';
+	echo '<link rel="stylesheet" type="text/css" href="skins/tiny-green/progressbar.css">';
+	echo '</head>'.PHP_EOL;
 	echo '<meta charset="UTF-8">'.PHP_EOL;
 	
+	# Process submittion
+	if(isset($_POST['bckFile'])){
+		echo '<div id="progressBar" class="tiny-green"><div></div></div>';
+		echo '<script>';
+		echo 'progressBar(75, $("#progressBar"));';
+		echo '</script>';
+	} else if(isset($_POST['bckData'])){
+		
+	}
+	
+	# Interface
 	echo 'BACKUP SYSTEM<br>'.PHP_EOL;
 	echo '<form action="saoluu.php" method="post">'.PHP_EOL;
 	echo '<table>'.PHP_EOL.'<tr>'.PHP_EOL;
